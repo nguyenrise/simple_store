0da99243b82fcd05b48068cf1f57c20bc05ca01c
Rise Nguyen <hnguyen33@rrc.ca>
2025-10-15 00:31:28 -0500

    Add faker and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Product.destroy_all
+676.times do
+  Product.create(
+    title: Faker::Commerce.product_name,
+    price: Faker::Commerce.price,
+    stock_quantity: Faker::Number.number(digits: 2)
+  )
+end
